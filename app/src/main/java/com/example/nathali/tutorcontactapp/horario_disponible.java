1d40acb897dbb880555875e2da081239eaf5a199
Drmsoul <Drmsoul>
2016-06-26 20:48:16 -0500

    Rafael Mayor, creacion actividad

---
+++ app/src/main/java/com/example/nathali/tutorcontactapp/horario_disponible.java
@@ -0,0 +1,13 @@
+package com.example.nathali.tutorcontactapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class horario_disponible extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horario_disponible);
+    }
+}
